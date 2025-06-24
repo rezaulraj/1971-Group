0d42383fe1ca73547a95469be1f47358b07b668d
rezaul-karim <rezaul.coderpro@gmail.com>
2025-06-24 13:07:03 +0600

    update hero image resolve commit

--- src/pages/home/HeroHome.jsx
+++ src/pages/home/HeroHome.jsx
@@ -31,7 +31,7 @@ const HeroHome = () => {
         {/* Gradient Background */}
         <div className="absolute inset-0 z-0 overflow-hidden">
           <div className="absolute inset-0 bg-gradient-to-br from-[#001153] via-[#1a2a6b] to-[#334380] opacity-95"></div>
-          <div className="absolute inset-0 bg-[url('../../assets/home/hero-pattern.svg')] bg-cover opacity-10"></div>
+          <div className="absolute inset-0 bg-cover opacity-10"></div>
         </div>
 
         <div className="container mx-auto px-4 relative z-10">
